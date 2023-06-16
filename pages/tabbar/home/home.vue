90b60f1a40fe91118252269be91e7f5864abac8b
huangzekun <huangzekun@cszysoft.com>
2023-06-16 18:45:56 +0800

    ✨ feat:login

--- pages/tabbar/home/home.vue
+++ pages/tabbar/home/home.vue
@@ -1,12 +1,23 @@
 <!-- 首页 -->
 <template>
-	 <text>首页</text>
+	<button open-type="getAuthorize" scope="userInfo" @getAuthorize="getPhoneNumber"
+		onError="handleAuthError">登录</button>
 </template>
 
 <script setup>
-
+const getPhoneNumber = () => {
+	my.getOpenUserInfo({
+		success: (res) => {
+			console.log('--res>>', JSON.parse(res.response).response);
+		},
+		fail: (err) => {
+			console.log(err)
+		}
+	});
+}
+const handleAuthError = (err) => {
+	 console.log('--err>>',err);
+}
 </script>
 
-<style lang="scss" scoped>
-	
-</style>
+<style lang="scss" scoped></style>
