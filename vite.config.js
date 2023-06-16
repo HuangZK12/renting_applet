91cbaf5c15e0506ed23782471a143ff825938722
huangzekun <huangzekun@cszysoft.com>
2023-06-16 15:37:55 +0800

    🎈 perf:unplugin-auto-import

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+//vite.config.js
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni"
+import AutoImport from "unplugin-auto-import/vite";
+import path from 'path';
+export default defineConfig({
+  plugins: [
+    uni(),
+    AutoImport({
+      imports: ["vue","./compositions"], 
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), //配置路径别名
+    },
+  },
+});
